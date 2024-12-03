31a084a0d23f56900d0f3e61388a151805228fa7
mlyward9 <mlyward9@gmail.com>
2024-12-03 23:50:08 +0800

    commit

--- databse/flowershop (1).sql
+++ databse/flowershop (1).sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Dec 02, 2024 at 03:05 PM
+-- Generation Time: Dec 03, 2024 at 04:49 PM
 -- Server version: 10.4.32-MariaDB
 -- PHP Version: 8.0.30
 
@@ -41,6 +41,14 @@ CREATE TABLE `shops` (
   `valid_id` varchar(255) DEFAULT NULL,
   `created_at` timestamp NOT NULL DEFAULT current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `shops`
+--
+
+INSERT INTO `shops` (`id`, `user_id`, `shop_name`, `shop_email`, `shop_address`, `shop_phone`, `profile_picture`, `identity_proof`, `business_license`, `sales_tax_registration`, `valid_id`, `created_at`) VALUES
+(4, 3, 'shop1', 'shop@gmail.com', 'Block 12 lot 32\r\nEksplorasyon', '0970860053', 'uploads/674dc79a83e22_674dc16491a7a_OIP.jpg', 'uploads/674dc79a83edc_674dc16491a7a_OIP.jpg', 'uploads/674dc79a83f90_674dc16491a7a_OIP.jpg', 'uploads/674dc79a84044_674dc16491a7a_OIP.jpg', 'uploads/674dc79a84111_674dc16491a7a_OIP.jpg', '2024-12-02 14:43:38'),
+(5, 3, 'shop2', 'shop@gmail.com', 'Block 12 lot 32\r\nEksplorasyon', '0970860053', 'uploads/674f269257173_674dcbcb2a00e_462555247_2787743184718289_1014975028182954442_n.jpg', 'uploads/674f26925afb5_674dcbcb2a00e_462555247_2787743184718289_1014975028182954442_n.jpg', 'uploads/674f26925b032_674dcbcb2a00e_462555247_2787743184718289_1014975028182954442_n.jpg', 'uploads/674f26925b0a6_674dcbcb2a00e_462555247_2787743184718289_1014975028182954442_n.jpg', 'uploads/674f26925b112_674dcbcb2a00e_462555247_2787743184718289_1014975028182954442_n.jpg', '2024-12-03 15:41:06');
 
 -- --------------------------------------------------------
 
@@ -93,7 +101,7 @@ ALTER TABLE `users`
 -- AUTO_INCREMENT for table `shops`
 --
 ALTER TABLE `shops`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 
 --
 -- AUTO_INCREMENT for table `users`
