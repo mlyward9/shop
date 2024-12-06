ca11b0c5a650c9773262d04b90e63f08a30534d8
juliusdonguines <juliusdonguines2018@gmail.com>
2024-12-06 22:10:08 +0800

    login

--- index.php
+++ index.php
@@ -39,27 +39,28 @@ ob_start();
         <h1></h1>
         <nav>
         <ul> 
-        <li><a href="index.php">Home</a></li>
-            <li><a href="index.php">About</a></li>
-            <li><a href="index.php">Contact</a></li>
-            <li><a href="map.php">Map</a></li>
-            <?php if (isset($_SESSION['user_id'])): ?>
-        <!-- If the user is logged in, show the My Account dropdown -->
-        <li><a href="logout.php">Logout</a></li>
-        <li><a href="shop.php">Your Shop</a></li>
-        <li class="dropdown">
-            <a href="#" class="dropbtn">My Account</a>
-            <div class="dropdown-content">
-                <a href="account_settings.php">Account Settings</a>
-                <a href="my_purchases.php">My Purchases</a>
-            </div>
-        </li>
-    <?php else: ?>
-        <!-- If the user is not logged in, show the Login link -->
-        <li><a href="login.php">Login</a></li>
-    <?php endif; ?>
-    <li><a href="view_cart.php">Cart</a></li>
-</ul>
+        <div class="main-nav">
+    <ul>
+        <li><a href="index.php" class="active">Home</a></li>
+        <li><a href="index.php">About</a></li>
+        <li><a href="index.php">Contact</a></li>
+        <li><a href="map.php">Map</a></li>
+        <?php if (isset($_SESSION['user_id'])): ?>
+            <li><a href="logout.php">Logout</a></li>
+            <li><a href="shop.php">Your Shop</a></li>
+            <li class="dropdown">
+                <a href="#" class="dropbtn">My Account</a>
+                <div class="dropdown-content">
+                    <a href="account_settings.php">Account Settings</a>
+                    <a href="my_purchases.php">My Purchases</a>
+                </div>
+            </li>
+        <?php else: ?>
+            <li><a href="login.php">Login</a></li>
+        <?php endif; ?>
+        <li><a href="view_cart.php" class="cart-btn">Cart</a></li>
+    </ul>
+</div>
 
 <style>
 /* Dropdown Container */
