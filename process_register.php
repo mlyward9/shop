ba65e5f54b441c3d9738df2c42ba2e6c64d59b86
mlyward9 <mlyward9@gmail.com>
2024-12-12 23:50:40 +0800

    add some sht

--- process_register.php
+++ process_register.php
@@ -5,13 +5,18 @@ require 'db.php'; // Include your database connection
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
     // Get the data from the form
     $firstname = mysqli_real_escape_string($conn, $_POST['firstname']);
-    $middlename = mysqli_real_escape_string($conn, $_POST['middlename']); // Add this
+    $middlename = mysqli_real_escape_string($conn, $_POST['middlename']);
     $lastname = mysqli_real_escape_string($conn, $_POST['lastname']);
     $username = mysqli_real_escape_string($conn, $_POST['username']);
     $birthday = mysqli_real_escape_string($conn, $_POST['birthday']);
     $email = mysqli_real_escape_string($conn, $_POST['email']);
     $password = mysqli_real_escape_string($conn, $_POST['password']);
     $repassword = mysqli_real_escape_string($conn, $_POST['repassword']);
+    $address = mysqli_real_escape_string($conn, $_POST['address']);
+    $barangay = mysqli_real_escape_string($conn, $_POST['barangay']);
+    $city = mysqli_real_escape_string($conn, $_POST['city']);
+    $province = mysqli_real_escape_string($conn, $_POST['province']);
+    $phone_number = mysqli_real_escape_string($conn, $_POST['phone_number']); // Added phone_number
 
     // Check if passwords match
     if ($password !== $repassword) {
@@ -31,8 +36,8 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     } else {
         // Insert the new user into the database
         $query = "
-            INSERT INTO users (firstname, middlename, lastname, username, birthday, email, password) 
-            VALUES ('$firstname', '$middlename', '$lastname', '$username', '$birthday', '$email', '$hashed_password')";
+            INSERT INTO users (firstname, middlename, lastname, username, birthday, email, password, address, barangay, city, province, phone_number) 
+            VALUES ('$firstname', '$middlename', '$lastname', '$username', '$birthday', '$email', '$hashed_password', '$address', '$barangay', '$city', '$province', '$phone_number')";
 
         if ($conn->query($query) === TRUE) {
             echo "Registration successful!";
